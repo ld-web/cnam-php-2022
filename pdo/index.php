86230e1057d762b231823f5bbbfdcd666357b3e1
Lucas LD-WEB <lucas@ld-web.net>
2022-09-12 11:33:06 +0200

    :sparkles: use "fetch" instead of "fetchAll"

--- pdo/index.php
+++ pdo/index.php
@@ -8,5 +8,15 @@ var_dump($pdo);
 $stmt = $pdo->query("SELECT * FROM users");
 var_dump($stmt);
 
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-var_dump($users);
+// fetchAll, sur un statement, permet de récupérer tous les éléments
+// d'un seul coup.
+// Ici, on lui indique qu'on souhaite que ces éléments soient des tableaux
+// associatifs, indexés sur les noms des colonnes de la table
+// $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($users);
+
+// Autre méthode : lire les éléments un par un
+// Dans ce cas, on n'utilise plus la méthode fetchAll, mais la méthode fetch
+while (($user = $stmt->fetch(PDO::FETCH_ASSOC)) !== false) {
+  var_dump($user);
+}
